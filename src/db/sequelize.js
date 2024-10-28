78c417a2b77e2be939eec6a4b1283086a5fc5558
Ehsan Mokhtari <e69m91@gmail.com>
2024-10-28 14:43:55 +0330

    fix(sequelize): change database connection for vercel connection error

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -2,7 +2,29 @@ const Sequelize = require("sequelize").Sequelize; // Import Sequelize constructo
 const SessionModel = require("./models/session"); // Import session model
 
 // Create a new Sequelize instance to connect to PostgreSQL
-const sequelize = new Sequelize(process.env.POSTGRES_URL)
+const sequelize = new Sequelize(process.env.POSTGRES_URL,{
+  dialect: 'postgres', // Database dialect
+  dialectModule: require('pg'), // PostgreSQL module
+})
+
+// Create a new Sequelize instance to connect to PostgreSQL
+/* const sequelize = new Sequelize({
+  dialect: 'postgres', // Database dialect
+  dialectModule: require('pg'), // PostgreSQL module
+  database: process.env.POSTGRES_DATABASE, // Database name from environment variables
+  username: process.env.POSTGRES_USER, // Username from environment variables
+  password: process.env.POSTGRES_PASSWORD, // Password from environment variables
+  host: process.env.POSTGRES_HOST, // Host from environment variables
+  port: 5432, // Default PostgreSQL port
+  dialectOptions: {
+    ssl: {
+      require: true, // Require SSL connection
+      rejectUnauthorized: false // Set to true in production for security
+    }
+  },
+  logging: console.log // Log SQL queries to the console
+});
+*/
 
 // Authenticate the connection to the database
 sequelize.authenticate().then(() => {
