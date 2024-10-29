34959956984af3bce25f960e735bb9c949027fc4
Ehsan Mokhtari <e69m91@gmail.com>
2024-10-29 09:59:25 +0330

    fix(sequelize): Change Sequelize instance

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -5,26 +5,8 @@ const SessionModel = require("./models/session"); // Import session model
 const sequelize = new Sequelize(process.env.POSTGRES_URL,{
   dialect: 'postgres', // Database dialect
   dialectModule: require('pg'), // PostgreSQL module
-})
-
-// Create a new Sequelize instance to connect to PostgreSQL
-/* const sequelize = new Sequelize({
-  dialect: 'postgres', // Database dialect
-  dialectModule: require('pg'), // PostgreSQL module
-  database: process.env.POSTGRES_DATABASE, // Database name from environment variables
-  username: process.env.POSTGRES_USER, // Username from environment variables
-  password: process.env.POSTGRES_PASSWORD, // Password from environment variables
-  host: process.env.POSTGRES_HOST, // Host from environment variables
-  port: 5432, // Default PostgreSQL port
-  dialectOptions: {
-    ssl: {
-      require: true, // Require SSL connection
-      rejectUnauthorized: false // Set to true in production for security
-    }
-  },
   logging: console.log // Log SQL queries to the console
-});
-*/
+})
 
 // Authenticate the connection to the database
 sequelize.authenticate().then(() => {
@@ -37,10 +19,8 @@ sequelize.authenticate().then(() => {
 const Session = SessionModel(sequelize, Sequelize);
 
 // Sync the database
-Session.sync();
 sequelize.sync();
 
 module.exports = {
-  sequelize, // Export the sequelize instance
   Session // Export the session model
 };
